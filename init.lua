de9c18aa7bdd09bb45d4b1b4c40dc04c65753663
robert <robert@arles.us>
2024-08-04 06:47:26 -0700

    mapping updates

--- init.lua
+++ init.lua
@@ -181,12 +181,17 @@ vim.opt.scrolloff = 10
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" }) -- TODO: <leader>c old
+vim.keymap.set("n", "<leader>lq", vim.diagnostic.setloclist, { desc = "[l]sp Open diagnostic [q]uickfix list" }) -- TODO: <leader>c new
 
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>ld[", vim.diagnostic.goto_prev, { desc = "Previous [D]iagnostic message (also <[d>)" })
+vim.keymap.set("n", "<leader>ld]", vim.diagnostic.goto_next, { desc = "Next [D]iagnostic message (also <]d>)" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" }) -- TODO: <leader>c old
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" }) -- TODO: <leader>c old
+vim.keymap.set("n", "<leader>le", vim.diagnostic.open_float, { desc = "[l]sp Show diagnostic [e]rror messages" }) -- TODO: <leader>c new
+vim.keymap.set("n", "<leader>lq", vim.diagnostic.setloclist, { desc = "[l]sp Open diagnostic [q]uickfix list" }) -- TODO: <leader>c new
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -317,11 +322,12 @@ require("lazy").setup({
 			require("which-key").add({
 				{ "<leader>c", group = "[C]ode" },
 				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>g", group = "[G]it", mode = { "n", "v" } },
+				{ "<leader>l", group = "[l]sp" },
 				{ "<leader>r", group = "[R]ename" },
 				{ "<leader>s", group = "[S]earch" },
 				{ "<leader>w", group = "[W]orkspace" },
 				{ "<leader>t", group = "[T]oggle" },
-				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
 			})
 		end,
 	},
@@ -513,43 +519,45 @@ require("lazy").setup({
 					-- Jump to the definition of the word under your cursor.
 					--  This is where a variable was first declared, or where a function is defined, etc.
 					--  To jump back, press <C-t>.
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("<leader>lD", require("telescope.builtin").lsp_definitions, "goto [D]efinition")
 
 					-- Find references for the word under your cursor.
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+					map("<leader>lR", require("telescope.builtin").lsp_references, "goto [R]eferences")
 
 					-- Jump to the implementation of the word under your cursor.
 					--  Useful when your language has ways of declaring types without an actual implementation.
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+					map("<leader>lI", require("telescope.builtin").lsp_implementations, "goto [I]mplementation")
 
 					-- Jump to the type of the word under your cursor.
 					--  Useful when you're not sure what type a variable is and you want to see
 					--  the definition of its *type*, not where it was *defined*.
-					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+					map("<leader>lT", require("telescope.builtin").lsp_type_definitions, "[T]ype definition")
 
 					-- Fuzzy find all the symbols in your current document.
 					--  Symbols are things like variables, functions, types, etc.
-					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+					map("<leader>lS", require("telescope.builtin").lsp_document_symbols, "document [S]ymbols")
 
 					-- Fuzzy find all the symbols in your current workspace.
 					--  Similar to document symbols, except searches over your entire project.
-					map(
-						"<leader>ws",
-						require("telescope.builtin").lsp_dynamic_workspace_symbols,
-						"[W]orkspace [S]ymbols"
-					)
+					map("<leader>lW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace symbols")
 
 					-- Rename the variable under your cursor.
 					--  Most Language Servers support renaming across files, etc.
-					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-
+					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame") --TODO: <leader>c old
+					map("<leader>lr", vim.lsp.buf.rename, "[l]sp [r]ename") --TODO: <leader>c new
 					-- Execute a code action, usually your cursor needs to be on top of an error
 					-- or a suggestion from your LSP for this to activate.
-					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
+					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction") -- TODO: <leader>c old
+					map("<leader>la", vim.lsp.buf.code_action, "[l]sp [a]ction") -- TODO: <leader>c new
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
 					--  For example, in C this would take you to the header.
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					-- map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration") -- TODO: <leader>c old
+					-- map("<leader>ld", vim.lsp.buf.definition, "goto [d]definition")
+					-- map("<leader>lD", vim.lsp.buf.declaration, "goto [D]eclaration") -- TODO: <leader>c new
+					-- map("<leader>lR", vim.lsp.buf.references, "find [R]references")
+					map("<leader>lh", vim.lsp.buf.hover, "[h]over documentation")
+					map("<leader>lH", vim.lsp.buf.signature_help, "signature [H]elp")
+					map("<leader>lf", vim.lsp.buf.format, "[f]ormat code")
 
 					-- The following two autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
