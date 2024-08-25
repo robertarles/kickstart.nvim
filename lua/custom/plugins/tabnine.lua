2859b9ccdddf4c90eb0b6a3d70612ac9214d2ff3
robert <robert@arles.us>
2024-08-25 09:57:10 -0700

    excluding tabnine on moriarty

--- lua/custom/plugins/tabnine.lua
+++ lua/custom/plugins/tabnine.lua
@@ -1,18 +1,26 @@
-return {
-	{
-		"codota/tabnine-nvim",
-		build = "./dl_binaries.sh",
-		config = function()
-			require("tabnine").setup({
-				disable_auto_comment = true,
-				accept_keymap = "<C-j>",
-				dismiss_keymap = "<C-k>",
-				debounce_ms = 800,
-				suggestion_color = { gui = "#808080", cterm = 244 },
-				exclude_filetypes = { "TelescopePrompt", "NvimTree" },
-				log_file_path = nil,
-				-- absolute path to Tabnine log file
-			})
-		end,
-	},
-}
+local hostname = vim.loop.os_gethostname()
+
+if not string.find(hostname, "moriarty") then
+	return {
+		{
+			"codota/tabnine-nvim",
+			build = "./dl_binaries.sh",
+			config = function()
+				require("tabnine").setup({
+					disable_auto_comment = true,
+					accept_keymap = "<C-j>",
+					dismiss_keymap = "<C-k>",
+					debounce_ms = 800,
+					suggestion_color = { gui = "#808080", cterm = 244 },
+					exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+					log_file_path = nil,
+					-- absolute path to Tabnine log file
+				})
+			end,
+		},
+	}
+else
+	-- dont run this on my weak ass linux box
+	return {}
+end
+
