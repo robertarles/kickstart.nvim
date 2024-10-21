49313b5f2f876a8c183efbe8eb153e270a346476
robert <robert@arles.us>
2024-10-21 06:07:02 -0700

    disable tabnine, test codeium

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -1,7 +1,11 @@
 local hostname = vim.loop.os_gethostname()
-if string.find(hostname, "moriarty") then
-	return {
-		"Exafunction/codeium.vim",
-		event = "BufEnter",
-	}
-end
+-- if string.find(hostname, "moriarty") then
+-- 	return {
+-- 		"Exafunction/codeium.vim",
+-- 		event = "BufEnter",
+-- 	}
+-- end
+return {
+	"Exafunction/codeium.vim",
+	event = "BufEnter",
+}
