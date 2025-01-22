ccab3b139081e3e3c2572695a22997cc0b4ffb72
robert <robert@arles.us>
2025-01-22 09:38:30 -0800

    update for d copilot

--- lua/custom/plugins/tabnine.lua
+++ lua/custom/plugins/tabnine.lua
@@ -1,24 +1,28 @@
 local hostname = vim.loop.os_gethostname()
 -- moriarty CPU spins out of control with tabnine running
 if not string.find(hostname, "moriarty") then
-	return {
-		{
-			"codota/tabnine-nvim",
-			build = "./dl_binaries.sh",
-			config = function()
-				require("tabnine").setup({
-					disable_auto_comment = true,
-					accept_keymap = "<Tab>",
-					dismiss_keymap = "<C-]>",
-					debounce_ms = 800,
-					suggestion_color = { gui = "#808080", cterm = 244 },
-					exclude_filetypes = { "TelescopePrompt", "NvimTree" },
-					log_file_path = nil,
-					ignore_certificate_errors = false,
-				})
-			end,
-		},
-	}
+	if not string.find(hostname, "C02FP34DMD6T") then
+		return {
+			{
+				"codota/tabnine-nvim",
+				build = "./dl_binaries.sh",
+				config = function()
+					require("tabnine").setup({
+						disable_auto_comment = true,
+						accept_keymap = "<Tab>",
+						dismiss_keymap = "<C-]>",
+						debounce_ms = 800,
+						suggestion_color = { gui = "#808080", cterm = 244 },
+						exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+						log_file_path = nil,
+						ignore_certificate_errors = false,
+					})
+				end,
+			},
+		}
+	else
+		return {}
+	end
 else
 	-- dont run this on my weak ass linux box
 	return {}
