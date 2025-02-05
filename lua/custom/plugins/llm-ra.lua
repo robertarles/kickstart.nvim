be4b74f640b8448f4359f307f3a0617a4a77ce71
robert <robert@arles.us>
2025-02-05 14:30:16 -0800

    exclude c02 from ollama

--- lua/custom/plugins/llm-ra.lua
+++ lua/custom/plugins/llm-ra.lua
@@ -1,43 +1,49 @@
-return {
-	"huggingface/llm.nvim",
-	opts = {
-		api_token = nil, -- cf Install paragraph
-		backend = "ollama", -- backend ID, "huggingface" | "ollama" | "openai" | "tgi"
-		url = "http://terminus.mooneye-blues.ts.net:11434", -- llm-ls uses "/api/generate"
-		tokens_to_clear = { "<|endoftext|>" }, -- tokens to remove from the model's output
-		-- parameters that are added to the request body, values are arbitrary, you can set any field:value pair here it will be passed as is to the backend
-		request_body = {
-			parameters = {
-				max_new_tokens = 60,
-				temperature = 0.2,
-				top_p = 0.95,
+local hostname = vim.loop.os_gethostname()
+-- moriarty CPU spins out of control with tabnine running
+if not string.find(hostname, "C02FP34DMD6T") then
+	return {
+		"huggingface/llm.nvim",
+		opts = {
+			api_token = nil, -- cf Install paragraph
+			backend = "ollama", -- backend ID, "huggingface" | "ollama" | "openai" | "tgi"
+			url = "http://terminus.mooneye-blues.ts.net:11434", -- llm-ls uses "/api/generate"
+			tokens_to_clear = { "<|endoftext|>" }, -- tokens to remove from the model's output
+			-- parameters that are added to the request body, values are arbitrary, you can set any field:value pair here it will be passed as is to the backend
+			request_body = {
+				parameters = {
+					max_new_tokens = 60,
+					temperature = 0.2,
+					top_p = 0.95,
+				},
 			},
+			-- set this if the model supports fill in the middle
+			fim = {
+				enabled = true,
+				prefix = "<fim_prefix>",
+				middle = "<fim_middle>",
+				suffix = "<fim_suffix>",
+			},
+			debounce_ms = 150,
+			accept_keymap = "<Tab>",
+			dismiss_keymap = "<S-Tab>",
+			tls_skip_verify_insecure = false,
+			-- llm-ls configuration, cf llm-ls section
+			lsp = {
+				bin_path = nil,
+				host = nil,
+				port = nil,
+				cmd_env = nil, -- or { LLM_LOG_LEVEL = "DEBUG" } to set the log level of llm-ls
+				version = "0.5.3",
+			},
+			tokenizer = nil, -- cf Tokenizer paragraph
+			context_window = 1024, -- max number of tokens for the context window
+			enable_suggestions_on_startup = true,
+			enable_suggestions_on_files = "*", -- pattern matching syntax to enable suggestions on specific files, either a string or a list of strings
+			disable_url_path_completion = false, -- cf Backend
+			-- cf Setup
+			model = "llama3.2:latest",
 		},
-		-- set this if the model supports fill in the middle
-		fim = {
-			enabled = true,
-			prefix = "<fim_prefix>",
-			middle = "<fim_middle>",
-			suffix = "<fim_suffix>",
-		},
-		debounce_ms = 150,
-		accept_keymap = "<Tab>",
-		dismiss_keymap = "<S-Tab>",
-		tls_skip_verify_insecure = false,
-		-- llm-ls configuration, cf llm-ls section
-		lsp = {
-			bin_path = nil,
-			host = nil,
-			port = nil,
-			cmd_env = nil, -- or { LLM_LOG_LEVEL = "DEBUG" } to set the log level of llm-ls
-			version = "0.5.3",
-		},
-		tokenizer = nil, -- cf Tokenizer paragraph
-		context_window = 1024, -- max number of tokens for the context window
-		enable_suggestions_on_startup = true,
-		enable_suggestions_on_files = "*", -- pattern matching syntax to enable suggestions on specific files, either a string or a list of strings
-		disable_url_path_completion = false, -- cf Backend
-		-- cf Setup
-		model = "llama3.2:latest",
-	},
-}
+	}
+else
+	return {}
+end
