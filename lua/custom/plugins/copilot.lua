1f7ff70c0802d4ff9f8955b3e3dc5790074a584d
robert <robert@arles.us>
2025-02-06 05:43:44 -0800

    test lowercase j k for accept/reject

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -5,8 +5,8 @@ if string.find(hostname, "C02FP34DMD6T") then
 			"github/copilot.vim",
 			config = function()
 				vim.g.copilot_no_tab_map = true
-				vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-				vim.api.nvim_set_keymap("i", "<C-K>", "copilot#Dismiss()", { silent = true, expr = true })
+				vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+				vim.api.nvim_set_keymap("i", "<C-k>", "copilot#Dismiss()", { silent = true, expr = true })
 			end,
 		},
 	}
