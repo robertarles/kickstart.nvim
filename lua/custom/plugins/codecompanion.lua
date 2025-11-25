5ff6e565f4dac4b20844b57055a42783d8807079
robert <robert@arles.us>
2025-11-25 06:38:12 -0800

    working codecompanion.nvim, using CLAUDE_CODE_OAUTH_TOKEN

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -1,41 +1,39 @@
 return {
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function()
-    require("codecompanion").setup({
-      adapters = {
-        http = {
-          anthropic = function()
-            return require("codecompanion.adapters").extend("anthropic", {
-              schema = {
-                model = {
-                  default = "claude-sonnet-4-20250514",
-                },
-              },
-            })
-          end,
-        },
-      },
-      strategies = {
-        chat = { adapter = "anthropic" },
-        inline = { adapter = "anthropic" },
-        cmd = { adapter = "anthropic" },
-      },
-      display = {
-        chat = {
-          window = {
-            layout = "vertical",
-          },
-        },
-      },
-    })
-  end,
-  keys = {
-    { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Toggle Chat" },
-    { "<leader>aa", "<cmd>CodeCompanionActions<cr>", desc = "Actions", mode = { "n", "v" } },
-    { "<leader>ai", "<cmd>CodeCompanion<cr>", desc = "Inline prompt", mode = { "n", "v" } },
-  },
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("codecompanion").setup({
+			adapters = {
+				acp = {
+					claude_code = function()
+						return require("codecompanion.adapters").extend("claude_code", {
+							env = {
+								CLAUDE_CODE_OAUTH_TOKEN = vim.env.CLAUDE_CODE_OAUTH_TOKEN,
+							},
+						})
+					end,
+				},
+			},
+			strategies = {
+				chat = { adapter = "claude_code" },
+				inline = { adapter = "claude_code" },
+				cmd = { adapter = "claude_code" },
+			},
+			display = {
+				chat = {
+					window = {
+						layout = "vertical",
+					},
+				},
+			},
+		})
+	end,
+	keys = {
+		{ "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Toggle Chat" },
+		{ "<leader>aa", "<cmd>CodeCompanionActions<cr>", desc = "Actions", mode = { "n", "v" } },
+		{ "<leader>ai", "<cmd>CodeCompanion<cr>", desc = "Inline prompt", mode = { "n", "v" } },
+	},
 }
